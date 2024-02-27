86b50ed49581f4b02838c2ca0e031cf8c60941b0
priyanshugarg19 <g.p.gargp.19@gmail.com>
2024-02-27 16:19:46 +0530

    create sidebar

---
+++ client/src/components/dashProfile.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function dashProfile() {
+  return (
+    <div>dashProfile</div>
+  )
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import {React, useState, useEffect } from 'react';
 import Header from '../components/header';
+import DashProfile from '../components/dashProfile';
+import DashSidebar from '../components/dashSidebar';
+import { useLocation } from 'react-router-dom';
+
+
+
 
 function Dashboard() {
+  const [tab , setTab]= useState("");
+  const location= useLocation();
+  
+  useEffect(()=>{
+
+    const params= new URLSearchParams(location.search);
+    const urlFromTabs = params.get('tab');
+    if(urlFromTabs){
+      setTab(urlFromTabs)
+    }
+
+  },[location.search])
+
+
   return (
     <>
     <Header />
-      <div className=''>
-        Dashboard
+      <div>
+
+        <DashSidebar />
+
+      </div>
+      <div>
+
+        {tab==='profile' && <DashProfile />}
+
       </div>
     </>
   )
