44c58e5224c20ee79f370e8a472edf42b8b220b4
priyanshugarg19 <g.p.gargp.19@gmail.com>
2024-03-07 17:17:35 +0530

    create a post section for sidebar, complete create a post functionality

--- client/src/components/dashSidebar.jsx
+++ client/src/components/dashSidebar.jsx
@@ -1,35 +1,50 @@
-import {React, useState} from 'react';
+import {React, useState, useEffect} from 'react';
 import { useDispatch, useSelector} from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { signOutSuccess } from '../redux/user/userSlice';
+import { IoMdLogOut } from "react-icons/io";
+
 function DashSidebar() {
-    const dispatch= useDispatch();
     const {currentUser} = useSelector(state=>state.user);
-    const [activeButton, setActiveButton] = useState(null);
+    const dispatch = useDispatch();
+    const [activeButton, setActiveButton] = useState('dashboard');
     const navigate = useNavigate();
+    const location = useLocation();
     const handleButtonClick = (buttonName) => {
         setActiveButton(buttonName);
         navigate(`/dashboard?tab=${buttonName}`);
     };
+    
+    useEffect(()=> {
+      const tab = window.location.href.split('/')[3];
+      if(tab === 'dashboard?tab=profile'){
+        setActiveButton('profile');
+      }
+      else if(tab === 'dashboard?tab=posts'){
+        setActiveButton('posts');
+      }
+    }, [location.search])
+  
 
-    const handleSignout=async ()=>{
+    const handleSignout = async()=> {
         try {
-            const res= await fetch('/api/user/signout',{
-                method : 'POST'
-            });
-            const data= await res.json();
-            if(!res.ok){
-                console.log(data.message);
-            }else{
-                dispatch(signOutSuccess(data))
-            }
+          const res = await fetch("/api/user/signout", {
+            method: 'POST'
+          });
+          const data = await res.json();
+          if(!res.ok){
+            console.log(data.message)
+          }
+          else{
+            dispatch(signOutSuccess());
+          }
         } catch (error) {
-            console.log(error);
+          console.log(error);
         }
-    }
+      }
 
   return (
-    <aside class="lg:flex lg:flex-col lg:w-64 w-screen lg:h-screen px-4 py-8 overflow-y-auto border-b-2 lg:border-r lg:rtl:border-r-0 lg:rtl:border-l dark:border-[rgb(35,39,42)] dark:bg-[rgb(35,39,42)]">
+    <aside class="lg:flex lg:flex-col lg:w-64 w-full lg:h-full px-4 py-8 overflow-y-auto border-b-2 lg:border-r lg:rtl:border-r-0 lg:rtl:border-l dark:border-[rgb(35,39,42)] dark:bg-[rgb(35,39,42)]">
             <a href="#" class="mx-auto">
                 <img class="w-auto h-6 sm:h-7" src="https://merakiui.com/images/full-logo.svg" alt="" />
             </a>
@@ -39,23 +54,39 @@ function DashSidebar() {
                 <h4 class="mx-2 mt-2 font-medium text-gray-800 dark:text-gray-200">{currentUser.username}</h4>
                 <p class="mx-2 mt-1 text-sm font-medium text-gray-600 dark:text-gray-400 ">{currentUser.email.slice(0,19)}....</p>
             </div>
-
+            <hr className='mt-8'></hr>
             <div class="flex flex-col justify-between flex-1 mt-6">
                 <nav>
-                    <button onClick={() => handleButtonClick('profile')} className={`flex items-center w-full lg:w-[220px] px-4 py-2 text-gray-600  rounded-lg  dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-600/20 dark:hover:text-gray-200 hover:text-gray-700 ${activeButton === 'profile' && 'dark:bg-gray-600/20 dark:text-white bg-gray-100'}`}>
+                    <button onClick={() => handleButtonClick('profile')} className={`flex items-center w-full lg:w-[220px] px-4 py-2 text-gray-600  rounded-lg  dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-600/20 dark:hover:text-gray-200 hover:text-gray-700 ${activeButton === 'profile' && 'dark:bg-gray-600/20 dark:text-white bg-gray-200'}`}>
                         <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M19 11H5M19 11C20.1046 11 21 11.8954 21 13V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V13C3 11.8954 3.89543 11 5 11M19 11V9C19 7.89543 18.1046 7 17 7M5 11V9C5 7.89543 5.89543 7 7 7M7 7V5C7 3.89543 7.89543 3 9 3H15C16.1046 3 17 3.89543 17 5V7M7 7H17" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
-
-                        <span class="mx-4 font-medium">Profile</span>
+                        <div className='flex flex-row justify-between w-full'>
+                          <span class="mx-4 font-medium">Profile</span>
+                          <span className={`${activeButton === 'profile' && 'dark:bg-[rgb(35,39,42)]/100'} dark:bg-gray-600 bg-sky-600 text-gray-200 font-semibold  text-[14px] rounded-lg w-14`}>{currentUser.isAdmin ? "admin" : "user"}</span>
+                        </div>
                     </button>
 
-                    <button onClick={handleSignout} class={`flex lg:w-[220px] w-full items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-600/20 dark:hover:text-gray-200 hover:text-gray-700 ${activeButton === 'signout' && 'dark:bg-gray-600/20 dark:text-white bg-gray-100'}`}>
+                    {/* <button onClick={() => handleButtonClick('accounts')} class={`flex lg:w-[220px] w-full items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-600/20 dark:hover:text-gray-200 hover:text-gray-700 ${activeButton === 'dashboard' && 'dark:bg-gray-600/20 text-white bg-gray-100'}`} >
                         <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M10.3246 4.31731C10.751 2.5609 13.249 2.5609 13.6754 4.31731C13.9508 5.45193 15.2507 5.99038 16.2478 5.38285C17.7913 4.44239 19.5576 6.2087 18.6172 7.75218C18.0096 8.74925 18.5481 10.0492 19.6827 10.3246C21.4391 10.751 21.4391 13.249 19.6827 13.6754C18.5481 13.9508 18.0096 15.2507 18.6172 16.2478C19.5576 17.7913 17.7913 19.5576 16.2478 18.6172C15.2507 18.0096 13.9508 18.5481 13.6754 19.6827C13.249 21.4391 10.751 21.4391 10.3246 19.6827C10.0492 18.5481 8.74926 18.0096 7.75219 18.6172C6.2087 19.5576 4.44239 17.7913 5.38285 16.2478C5.99038 15.2507 5.45193 13.9508 4.31731 13.6754C2.5609 13.249 2.5609 10.751 4.31731 10.3246C5.45193 10.0492 5.99037 8.74926 5.38285 7.75218C4.44239 6.2087 6.2087 4.44239 7.75219 5.38285C8.74926 5.99037 10.0492 5.45193 10.3246 4.31731Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                            <path d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M12 14C8.13401 14 5 17.134 5 21H19C19 17.134 15.866 14 12 14Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
 
+                        <span class="mx-4 font-medium">Accounts</span>
+                    </button>*/}
+
+                    { currentUser.isAdmin &&
+                      <button onClick={() => handleButtonClick('posts')} class={`flex lg:w-[220px] w-full items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-600/20 dark:hover:text-gray-200 hover:text-gray-700 ${activeButton === 'posts' && 'dark:bg-gray-600/20 dark:text-white bg-gray-200'}`} href="#">
+                        <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M15 5V7M15 11V13M15 17V19M5 5C3.89543 5 3 5.89543 3 7V10C4.10457 10 5 10.8954 5 12C5 13.1046 4.10457 14 3 14V17C3 18.1046 3.89543 19 5 19H19C20.1046 19 21 18.1046 21 17V14C19.8954 14 19 13.1046 19 12C19 10.8954 19.8954 10 21 10V7C21 5.89543 20.1046 5 19 5H5Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                        <span class="mx-4 font-medium">Posts</span>
+                      </button> 
+                    }
+
+                    <button onClick={handleSignout} class={`flex lg:w-[220px] w-full items-center px-[10px] py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-600/20 dark:hover:text-gray-200 hover:text-gray-700 ${activeButton === 'signout' && 'dark:bg-gray-600/20 dark:text-white bg-gray-100'}`}>
+                        <IoMdLogOut className='w-[30px] h-[22px] ' />
                         <span class="mx-4 font-medium">Sign Out</span>
                     </button>
                 </nav>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,42 +1,40 @@
-import {React, useState, useEffect } from 'react';
+import {React, useState, useEffect} from 'react';
 import Header from '../components/header';
-import DashProfile from '../components/dashProfile';
-import DashSidebar from '../components/dashSidebar';
 import { useLocation } from 'react-router-dom';
-
-
-
-
+import DashProfile from '../components/dashProfile';
+import DashSidebar from '../components/DashSidebar';
+import DashPosts from '../components/DashPosts';
 function Dashboard() {
-  const [tab , setTab]= useState("");
-  const location= useLocation();
-  
-  useEffect(()=>{
+  const location = useLocation();
+  const [tab, setTab] = useState('');
 
-    const params= new URLSearchParams(location.search);
-    const urlFromTabs = params.get('tab');
-    if(urlFromTabs){
-      setTab(urlFromTabs)
+  useEffect(()=> {
+    const params = new URLSearchParams(location.search);
+    const tabsFromUrl = params.get('tab');
+    if(tabsFromUrl){
+      setTab(tabsFromUrl);
     }
-
-  },[location.search])
+  }, [location.search])
 
 
   return (
+
     <>
-      <Header />
-
+    <Header />
+      {/* sidebar */}
       <div className='min-h-screen flex flex-col lg:flex-row'>
-      <div>
-
-        <DashSidebar />
-
-      </div>
-      <div className=' w-full min-h-[700px] lg:min-h-screen'>
-
-        {tab==='profile' && <DashProfile />}
-
-      </div>
+        <div>
+          <DashSidebar />
+        </div>
+        {/* profile */}
+        <div className='w-full min-h-[700px] lg:min-h-screen'>
+          {
+            tab === 'profile' && <DashProfile />
+          }
+          {
+            tab === 'posts' && <DashPosts />
+          }
+        </div>
       </div>
     </>
   )
