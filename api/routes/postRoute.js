eb76cd5304dd5de39526bf863fb21690c0149151
priyanshugarg19 <g.p.gargp.19@gmail.com>
2024-03-07 18:22:41 +0530

    create getPost api route

--- api/routes/postRoute.js
+++ api/routes/postRoute.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { verifyUser } from "../utils/verifyUser.js";
 import { create } from "../Controllers/postController.js";
-
+import {getPosts} from "../Controllers/postController.js"
 const router = express.Router();
 
 router.post('/create', verifyUser, create )
-
+router.get('/getposts', getPosts);
 export default router;
