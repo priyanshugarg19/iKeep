4ce9fa1af43a8db7bf711241092a22a2f49fb220
priyanshugarg19 <g.p.gargp.19@gmail.com>
2024-03-25 19:28:37 +0530

    create likeComment api

--- api/Controllers/commentController.js
+++ api/Controllers/commentController.js
@@ -37,3 +37,25 @@ export const getComments =async (req, res, next)=>{
         next(error)
     }
 }
+
+export const likeComment = async (req, res, next)=>{
+    try {
+        const comment = await Comment.findById(req.params.id);
+        if(!comment){
+            next(errorHandler(404, "Comment not found"));
+        }
+        const userIndex=  comment.likes.indexOf(req.user.id);
+        if(userIndex === -1){
+            comment.numberOfLikes += 1 
+            comment.likes.push(req.user.id);
+        }
+        else{
+            comment.numberOfLikes -= 1 
+            comment.likes.splice(userIndex, 1);
+        }
+        await comment.save();
+        res.status(200).json(comment);
+    } catch (error) {
+        next(error)
+    }
+}
--- api/routes/commentRoute.js
+++ api/routes/commentRoute.js
@@ -1,9 +1,9 @@
 import  express  from "express";
 import { verifyUser } from "../utils/verifyUser.js";
-import {createComment, getComments} from "../Controllers/commentController.js"
+import {createComment, getComments, likeComment} from "../Controllers/commentController.js"
 const router= express.Router();
 
 router.post('/create', verifyUser, createComment);
 router.get('/getcomments/:postId', getComments);
-
+router.put('/commentlike/:commentid', verifyUser, likeComment);
 export default router;
