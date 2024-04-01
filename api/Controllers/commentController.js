8142f434a2c8062d6518e85afb6b0bc8308c6e77
priyanshugarg19 <g.p.gargp.19@gmail.com>
2024-04-01 14:17:46 +0530

    create get all comments Api route

--- api/Controllers/commentController.js
+++ api/Controllers/commentController.js
@@ -94,3 +94,28 @@ export const deleteComment = async (req, res, next) => {
         next(error);
     }
 };
+
+export const getAllComments = async(req, res, next)=>{
+    if(!req.user.isAdmin){
+        next(errorHandler(403, "Not authorized to get comments"));
+    }
+    
+    try {
+
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 10;
+        const sortDirection = req.query.sort == 'desc' ? -1 : 1;
+        const comments = await Comment.find().sort({createdAt : sortDirection}).skip(startIndex).limit(limit);
+
+        const totalComments= await Comment.countDocuments();
+
+        const now = new Date();
+        const oneMonthago = new Date(now.getFullYear(), now.getMonth()-1, now.getDate());
+        const lastMonthComments = await Comment.countDocuments({createdAt :{$gte : oneMonthago }});
+
+        res.status(200).json({comments, totalComments, lastMonthComments});
+
+    } catch (error) {
+        next(error);
+    }
+}
--- api/routes/commentRoute.js
+++ api/routes/commentRoute.js
@@ -1,6 +1,6 @@
 import  express  from "express";
 import { verifyUser } from "../utils/verifyUser.js";
-import {createComment, getComments, likeComment, editComment, deleteComment} from "../Controllers/commentController.js"
+import {createComment, getComments, likeComment, editComment, deleteComment, getAllComments} from "../Controllers/commentController.js"
 const router= express.Router();
 
 router.post('/create', verifyUser, createComment);
@@ -8,5 +8,5 @@ router.get('/getcomments/:postId', getComments);
 router.put('/commentlike/:commentid', verifyUser, likeComment);
 router.put('/commentedit/:commentid', verifyUser, editComment);
 router.delete('/commentdelete/:commentid', verifyUser, deleteComment);
-
+router.get('/getallcomments', verifyUser, getAllComments)
 export default router;
