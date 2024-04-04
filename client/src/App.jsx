57af251435d32a9d09f2e3cde2cc190c5e5f5a77
priyanshugarg19 <g.p.gargp.19@gmail.com>
2024-04-04 20:00:07 +0530

    complete home page ui functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,42 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import About from './pages/About';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Dashboard from './pages/Dashboard';
 import Header from './components/header';
+// import Footer from './components/footer.jsx';
 import PrivateRoute from './components/privateRoute';
-import AdminPrivateRoute from './components/adminPrivateRoute';
+import AdminPostPrivateRoute from './components/adminPrivateRoute';
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
 import PostPage from './pages/PostPage';
+
 const App = () => {
   // const location = useLocation();
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<About />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />}/>
         <Route path="/sign-up" element={<SignUp />} />
-        <Route element = {<PrivateRoute />}>
+        <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
         </Route>
-        <Route path="/projects" element={<Projects />} />
-        <Route path='/about' element={<About />} />
-        <Route element={<AdminPrivateRoute />}>
-          <Route path= "/CreatePost" element={<CreatePost />} />
-          <Route path= "/UpdatePost/:postId" element={<UpdatePost />} />
+        <Route element={<AdminPostPrivateRoute />}>
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:postId" element={<UpdatePost />} />
+
         </Route>
-        <Route path = "/post/:postSlug" element= {<PostPage />}></Route>
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/post/:postSlug" element={<PostPage />} />
+        <Route path='/about' element={<About />} />
       </Routes>
+      {/* <Footer /> */}
     </BrowserRouter>
   )
 }
